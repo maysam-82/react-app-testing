0bb62514d9a99c0893114dea3d3616b9afc2d962
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-24 02:36:26 +1000

    change to class component with states and events

--- src/Components/CommentBox/CommentBox.js
+++ src/Components/CommentBox/CommentBox.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-	return <div>I am CommentBox</div>;
-};
+class CommentBox extends Component {
+	state = {
+		comment: '',
+	};
+
+	handleChange = (event) => {
+		this.setState({ comment: event.target.value });
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		//TODO: call an action creator and save the comment.
+		this.setState({ comment: '' });
+	};
+
+	render() {
+		const { comment } = this.state;
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<h4>Add a Comment</h4>
+					<textarea value={comment} onChange={this.handleChange} />
+					<div>
+						<button>Submit</button>
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default CommentBox;
