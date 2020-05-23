0daf43aa93a92401b042018e8574681b9b55efdb
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-24 03:20:43 +1000

    write test for event handling

--- src/Components/CommentBox/__tests__/CommentBox.test.js
+++ src/Components/CommentBox/__tests__/CommentBox.test.js
@@ -15,3 +15,17 @@ it('should have a textarea and button', () => {
 	expect(component.find('textarea').length).toEqual(1);
 	expect(component.find('button').length).toEqual(1);
 });
+
+it('should have a textarea that users can type in', () => {
+	const textare = component.find('textarea');
+	// to simulate a change event we use `simulate()` method provided by Enzyme.
+	// `change` is actual name of event as an HTML event not react one.
+	// The second argument of `simulate()` method is a fake event object which acts like a `event.target.value or `{target: { value: 'new comment' }}`
+	textare.simulate('change', {
+		target: { value: 'new comment' },
+	});
+	// Forcing component to update
+	component.update();
+	// Making sure that the textarea receives the correct `value` prop.
+	expect(component.find('textarea').prop('value')).toEqual('new comment');
+});
