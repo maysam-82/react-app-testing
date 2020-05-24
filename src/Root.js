8425f560ced0cab3324fd461b9782675ae895a5a
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-24 23:39:03 +1000

    add applyMiddleware to Root

--- src/Root.js
+++ src/Root.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from 'reducers';
-
+import thunk from 'redux-thunk';
 // Set default value for initialState to `{}` since the app will not use that and we only use `initialState` for testing purpose.
 export default ({ children, initialState = {} }) => {
 	// When redux is going to create an store, we can optionally provide initialState for starting app.
-	return (
-		<Provider store={createStore(reducers, initialState)}>{children}</Provider>
-	);
+	const store = createStore(reducers, initialState, applyMiddleware(thunk));
+	return <Provider store={store}>{children}</Provider>;
 };
