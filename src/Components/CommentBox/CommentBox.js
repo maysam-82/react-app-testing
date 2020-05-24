d4292bcfa6a298f4f6fb2ee676947bba31cd4c9f
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-24 21:35:43 +1000

    convert class component to functional with hooks

--- src/Components/CommentBox/CommentBox.js
+++ src/Components/CommentBox/CommentBox.js
@@ -1,36 +1,31 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { saveComment } from 'actions/actionCreators';
 import { connect } from 'react-redux';
 
-class CommentBox extends Component {
-	state = {
-		comment: '',
+const CommentBox = (props) => {
+	const [comment, setComment] = useState('');
+
+	const handleChange = (event) => {
+		setComment(event.target.value);
 	};
 
-	handleChange = (event) => {
-		this.setState({ comment: event.target.value });
-	};
-
-	handleSubmit = (event) => {
+	const handleSubmit = (event) => {
 		event.preventDefault();
-		this.props.saveComment(this.state.comment);
-		this.setState({ comment: '' });
+		props.saveComment(comment);
+		setComment('');
 	};
 
-	render() {
-		const { comment } = this.state;
-		return (
-			<div>
-				<form onSubmit={this.handleSubmit}>
-					<h4>Add a Comment</h4>
-					<textarea value={comment} onChange={this.handleChange} />
-					<div>
-						<button>Submit</button>
-					</div>
-				</form>
-			</div>
-		);
-	}
-}
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<h4>Add a Comment</h4>
+				<textarea value={comment} onChange={handleChange} />
+				<div>
+					<button>Submit</button>
+				</div>
+			</form>
+		</div>
+	);
+};
 
 export default connect(null, { saveComment })(CommentBox);
