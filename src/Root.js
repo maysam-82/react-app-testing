8b6e324d1f0a0f7347a85b4a219af2eae041bfb0
maysam82.dev@gmail.com <maysam82.dev@gmail.com>
2020-05-24 22:18:42 +1000

    update Root to add initialState to redux for test

--- src/Root.js
+++ src/Root.js
@@ -3,8 +3,10 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from 'reducers';
 
-const store = createStore(reducers);
-
-export default ({ children }) => {
-	return <Provider store={store}>{children}</Provider>;
+// Set default value for initialState to `{}` since the app will not use that and we only use `initialState` for testing purpose.
+export default ({ children, initialState = {} }) => {
+	// When redux is going to create an store, we can optionally provide initialState for starting app.
+	return (
+		<Provider store={createStore(reducers, initialState)}>{children}</Provider>
+	);
 };
